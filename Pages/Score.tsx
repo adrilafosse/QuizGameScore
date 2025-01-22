8cbc42d6e78dc0ebeb14a3cc37429fd602eec59e
adrilafosse <81637698+adrilafosse@users.noreply.github.com>
2025-01-22 22:47:57 +0100

    ok

--- Pages/Score.tsx
+++ Pages/Score.tsx
@@ -7,11 +7,11 @@ import { useRoute } from '@react-navigation/native';
 import { Platform, Dimensions } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 
-const {width} = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
 interface RouteParams {
     valeur: string;
-  }
+}
 
 const Score: React.FC<{ navigation: any }> = ({ navigation }) => {
     const route = useRoute();
@@ -23,27 +23,26 @@ const Score: React.FC<{ navigation: any }> = ({ navigation }) => {
     const [nouvelleQuestion, setNouvelleQuestion] = useState(false);
     const [QRcode, setQRcode] = useState(false);
     const uniqueId = valeur;
-    const qrcode = `https://storage.googleapis.com/quizgame/web-build/index.html?id=${uniqueId}`;
-
+    const qrcode = `https://storage.googleapis.com/quizgame/index.html?id=${uniqueId}`;
     useEffect(() => {
         get(ref(db, `${valeur}/question-temps`)).then((snapshot) => {
             if (snapshot.exists()) {
                 const data = snapshot.val();
                 const dataFormatter = Object.entries(data).map(([key, value]) => ({
                     question: key,
-                    date: value  
+                    date: value
                 }));
                 setDateQuestion(dataFormatter);
             }
         });
     }, []);
-    
+
     useEffect(() => {
         dateQuestion.forEach((dateStr) => {
             const date = new Date(dateStr.date);
             const delay = date.getTime() + 120000 - Date.now();
             if (delay > 0) {
-            //setTimeout se declenche quand delay arrive a 0
+                //setTimeout se declenche quand delay arrive a 0
                 setTimeout(() => {
                     get(ref(db, `${valeur}/score`)).then((snapshot) => {
                         if (snapshot.exists()) {
@@ -51,7 +50,7 @@ const Score: React.FC<{ navigation: any }> = ({ navigation }) => {
                             const dataArray = Object.entries(nouvelleDonnee)
                                 .map(([name, score]) => ({ name, score: Number(score) }))
                                 .sort((a, b) => b.score - a.score);
-                                setDataTableau(dataArray);
+                            setDataTableau(dataArray);
                         } else {
                             console.log('Aucune donnée trouvée !');
                         }
@@ -69,49 +68,49 @@ const Score: React.FC<{ navigation: any }> = ({ navigation }) => {
                 console.log('La date est déjà passée');
             }
             const delay2 = date.getTime() - Date.now();
-            if(delay2 > 0){
+            if (delay2 > 0) {
                 setTimeout(() => {
                     setNouvelleQuestion(true);
                 }, delay2);
             }
         });
-      }, [dateQuestion]);
+    }, [dateQuestion]);
 
     return (
         <View style={styles.container}>
             {nouvelleQuestion === true ? (
                 <Text style={styles.titre}>Une nouvelle question est apparue !</Text>
             ) :
-            <>
-                { QRcode === false &&  question === '' ? (
                 <>
-                <Text style={styles.titre}>Score</Text>
-                {question !== '' && bonneReponse !== '' ? (
-                    <Text style={styles.sous_titre}>{question} : {bonneReponse}</Text>
-                ) : null}
-                {dataTableau.map((item, index) => (
-                    <View key={item.name} style={styles.rankItem}>
-                        <Text style={styles.rankText}>{index + 1}. {item.name} - {item.score} pts</Text>
-                    </View>
-                ))}
-                {question === '' ? (
-                <TouchableOpacity style={styles.bouton2} onPress={() => setQRcode(true)}>
-                    <Text style={styles.boutonText}>Afficher le QRcode de la partie</Text>
-                </TouchableOpacity>
-                ) : null}
+                    {QRcode === false && question === '' ? (
+                        <>
+                            <Text style={styles.titre}>Score</Text>
+                            {question !== '' && bonneReponse !== '' ? (
+                                <Text style={styles.sous_titre}>{question} : {bonneReponse}</Text>
+                            ) : null}
+                            {dataTableau.map((item, index) => (
+                                <View key={item.name} style={styles.rankItem}>
+                                    <Text style={styles.rankText}>{index + 1}. {item.name} - {item.score} pts</Text>
+                                </View>
+                            ))}
+                            {question === '' ? (
+                                <TouchableOpacity style={styles.bouton2} onPress={() => setQRcode(true)}>
+                                    <Text style={styles.boutonText}>Afficher le QRcode de la partie</Text>
+                                </TouchableOpacity>
+                            ) : null}
+                        </>
+                    ) :
+                        <>
+                            <Text style={styles.titre}>Pour rejoindre la partie tout nouveau joueur peut scanner ce QRcode :</Text>
+                            <View style={styles.qrContainer}>
+                                <QRCode value={qrcode} size={Platform.OS === 'web' && width >= 768 ? wp('20%') : hp('40%')} />
+                            </View>
+                            <TouchableOpacity style={styles.bouton2} onPress={() => setQRcode(false)}>
+                                <Text style={styles.boutonText}>Retour</Text>
+                            </TouchableOpacity>
+                        </>
+                    }
                 </>
-                ) : 
-                <>
-                  <Text style={styles.titre}>Pour rejoindre la partie tout nouveau joueur peut scanner ce QRcode :</Text>
-                  <View style={styles.qrContainer}>
-                    <QRCode value={qrcode} size={Platform.OS === 'web' && width >= 768 ? wp('20%') :  hp('40%')} />
-                  </View>
-                  <TouchableOpacity style={styles.bouton2} onPress={() => setQRcode(false)}>
-                    <Text style={styles.boutonText}>Retour</Text>
-                  </TouchableOpacity>
-                </>
-                }
-            </>
             }
         </View>
     )
@@ -121,7 +120,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#F5F5F5',
         alignItems: 'center',
-        paddingTop: Platform.OS === 'web' && width >= 768 ? wp('1%') :  wp('2%'),
+        paddingTop: Platform.OS === 'web' && width >= 768 ? wp('1%') : wp('2%'),
     },
     bouton2: {
         backgroundColor: '#4CAF50',
@@ -131,26 +130,26 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === 'web' && width >= 768 ? wp('2%') : wp('8%'),
         alignItems: 'center',
         justifyContent: 'center',
-      },
-      qrContainer: {
-        marginVertical: Platform.OS === 'web' && width >= 768 ? hp('5%') :  hp('2%'),
+    },
+    qrContainer: {
+        marginVertical: Platform.OS === 'web' && width >= 768 ? hp('5%') : hp('2%'),
         justifyContent: 'center',
         alignItems: 'center',
-      },
-      boutonText: {
+    },
+    boutonText: {
         color: '#FFFFFF',
-        fontSize:  Platform.OS === 'web' && width >= 768 ? wp('1.5%') : wp('4%'),
+        fontSize: Platform.OS === 'web' && width >= 768 ? wp('1.5%') : wp('4%'),
         fontWeight: 'bold',
-      },
+    },
     sous_titre: {
         color: 'red',
         textAlign: 'center',
-        fontSize: Platform.OS === 'web' && width >= 768 ? wp('2%') :  wp('4%'),
-        paddingTop: Platform.OS === 'web' && width >= 768 ? wp('2%') :  hp('3%'),
-        paddingBottom : Platform.OS === 'web' && width >= 768 ? wp('2%') :  wp('5%'),
+        fontSize: Platform.OS === 'web' && width >= 768 ? wp('2%') : wp('4%'),
+        paddingTop: Platform.OS === 'web' && width >= 768 ? wp('2%') : hp('3%'),
+        paddingBottom: Platform.OS === 'web' && width >= 768 ? wp('2%') : wp('5%'),
         textDecorationLine: 'underline',
-        paddingHorizontal: Platform.OS === 'web' && width >= 768 ? wp('2%') :  wp('5%'),
-      },
+        paddingHorizontal: Platform.OS === 'web' && width >= 768 ? wp('2%') : wp('5%'),
+    },
     rankItem: {
         backgroundColor: '#fff',
         borderRadius: 8,
@@ -172,7 +171,7 @@ const styles = StyleSheet.create({
     titre: {
         color: '#333333',
         fontWeight: 'bold',
-        fontSize: Platform.OS === 'web' && width >= 768 ? wp('3%') :  wp('8%'),
+        fontSize: Platform.OS === 'web' && width >= 768 ? wp('3%') : wp('8%'),
     },
 });
 export default Score;
